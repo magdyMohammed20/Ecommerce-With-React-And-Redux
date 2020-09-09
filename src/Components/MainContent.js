8a460889c52889c7c611f25f3612c8fa5dc3adcd
magdyMohammed20 <magdymohammed37@yahoo.com>
2020-09-09 13:22:00 +0200

    Fetch Products Data And Setup Products Structure

--- src/Components/MainContent.js
+++ src/Components/MainContent.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import data from '../data.json'
+import {useState,useEffect} from 'react'
 
 function MainContent() {
+    const [products , setProducts] = useState([])
+    const [size , setSize] = useState("")
+    const [sort , setSort] = useState("")
+
+    useEffect(() => {
+        setProducts(data.products)
+    })    
+
+    console.log(data)
     return (
-        <main>
-            Product List
-        </main>
+        <div className='main'>
+            <div className='products'>
+                Product List
+            </div>
+            <div className='sidebar'>
+                Sidebar
+            </div>
+        </div>
     )
 }
 
