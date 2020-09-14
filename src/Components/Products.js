e2e0bfe287b0ca1683027ee79d780e86a8d50d7f
magdyMohammed20 <magdymohammed37@yahoo.com>
2020-09-14 23:41:51 +0200

    Create Product Modal

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,15 +1,72 @@
-import React from 'react'
+import React , {useState} from 'react'
 import Fade from 'react-reveal/Fade'
+import Zoom from 'react-reveal/Zoom'
+import Modal from 'react-modal'
+
 function Products({products , addToCart}) {
+    const [currentProduct , setCurrentProduct] = useState(null)
+    const [modalIsOpen , setModalIsOpen] = useState(false)
+
+    const openModal = product => {
+        setCurrentProduct(product)
+        setModalIsOpen(true)
+    }
+
+    const closeModal = _ => {
+        setCurrentProduct(null)
+        setModalIsOpen(false)
+    }
+
     return (
         <Fade bottom cascade>
-            <div>
+            <div className='showProduct'>
             {
                 products.length > 0 ? (
                     products.map(product => {
                         return <li key={product.id}>
+
+                            <div>
+                            {
+                                currentProduct && (
+                                    <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
+                                        <Zoom>
+                                            <button className='close-modal' onClick={closeModal}>
+                                                x
+                                            </button>
+                                            <div className='product-details3'>
+                                                <img src={currentProduct.image} alt={currentProduct.title}/>
+                                                <div className='product-data'>
+                                                    <strong>{currentProduct.title}</strong>
+                                                    <p className='product-desc'>{currentProduct.description}</p>
+                                                    <p className='sizes'>
+                                                        Available Sizes : 
+                                                        {
+                                                            currentProduct.availableSizes.map(x => {
+                                                                return <span>
+                                                                    <button className='primary size'>{x}</button>
+                                                                </span>
+                                                            })
+                                                        }
+                                                    </p>
+                                                    <div className='panda'>
+                                                        <p>Price : ${Math.ceil(currentProduct.price)}</p>
+                                                        <button className='primary size' onClick={() => {
+                                                            addToCart(currentProduct)
+                                                            closeModal()
+                                                        }}>
+                                                            Add To Cart
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </Zoom>
+                                    </Modal>
+                                )
+                            }
+                            </div>
+
                             <div className='product-data'>
-                                <a href='#'>
+                                <a href={'#' + product.id} onClick={() => openModal(product)}>
                                     <img src={product.image} alt={product.title}/>
                                     <p>{product.title}</p>
                                 </a>
@@ -27,8 +84,10 @@ function Products({products , addToCart}) {
                 )
             }
             </div>
+            
         </Fade>
     )
 }
 
+Modal.setAppElement('body');
 export default Products
