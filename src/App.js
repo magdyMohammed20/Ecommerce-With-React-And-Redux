f6a6ded3d707c2f46fab89e3b6e4e45499bf1387
magdyMohammed20 <magdymohammed37@yahoo.com>
2020-09-16 12:27:48 +0200

    Complete Adding Redux To Application
    - Replace Data Comes From Json With Data Comes From Api And Redux Store
    - Having Issue Of Filter Functionality Not Working Correctly

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import Header from './Components/Header'
 import MainContent from './Components/MainContent'
 import Footer from './Components/Footer'
+import {Provider} from 'react-redux'
+import store from './Redux/store'
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <MainContent/>
-      <Footer/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Header/>
+        <MainContent/>
+        <Footer/>
+      </div>
+    </Provider>
   );
 }
 
